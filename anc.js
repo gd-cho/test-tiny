5ac1249d67b8f11ca6e86ee4c00cfb332e5c7872
gd-cho <zhouguangdong494@gmail.com>
2022-07-27 14:46:47 +0800

    稳定版

--- anc.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.abc = void 0;
-function abc(a, b) {
-    return a + b;
-}
-exports.abc = abc;
--- anc.ts
+++
@@ -1,3 +0,0 @@
-export function abc(a: number, b: number) {
-  return a + b;
-}
