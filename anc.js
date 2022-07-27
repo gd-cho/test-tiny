a267386fba38a8c40723e3e56a0d9cfe1fb69afd
gd-cho <zhouguangdong494@gmail.com>
2022-07-27 14:29:33 +0800

    新增字符串工具类

---
+++ anc.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.abc = void 0;
+function abc(a, b) {
+    return a + b;
+}
+exports.abc = abc;
---
+++ anc.ts
@@ -0,0 +1,3 @@
+export function abc(a: number, b: number) {
+  return a + b;
+}
